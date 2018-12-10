e4b00d5d0114940d424e655694e3f868c9d37802
Aramex <slavbez@mail.ru>
2018-12-11 00:47:35 +0200

    button

--- js/script.js
+++ js/script.js
@@ -1,5 +1,19 @@
 $(document).ready(function () {
 
+    $('.away').click(function () {
+       $(this).toggleClass('away-move');
+        $('.home').removeClass('home-move');
+        $('.home').css('background-color','unset');
+        $(".home-wrap").fadeOut(500);
+        $('.away-wrap').fadeIn(500);
+    });
+    $('.home').click(function () {
+        $(this).addClass('home-move');
+        $('.away').removeClass('away-move');
+        $(".away-wrap").fadeOut(500);
+        $('.home-wrap').fadeIn(500);
+    });
+
 
 
 
